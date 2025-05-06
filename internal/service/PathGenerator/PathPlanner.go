1f4fea9bdaf0bf6ddf026588e12ee5f8ee90f1f7
husseinkhaled <husseinkhaled733@gmail.com>
2025-05-07 01:07:26 +0300

    Update interfaces in CandidateGenerator, MaximumMatching, and PathPlanner to use pointers for offers and requests

--- internal/service/PathGenerator/PathPlanner.go
+++ internal/service/PathGenerator/PathPlanner.go
@@ -3,5 +3,5 @@ package PathGenerator
 import "matching-engine/internal/model"
 
 type PathPlanner interface {
-	findFirstFeasiblePath(offerNode model.OfferNode, requestNode model.RequestNode) ([]model.Point, bool, error)
+	findFirstFeasiblePath(offerNode *model.OfferNode, requestNode *model.RequestNode) ([]*model.Point, bool, error)
 }
