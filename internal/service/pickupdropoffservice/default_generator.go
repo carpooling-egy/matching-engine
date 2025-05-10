0e71fa090a08903283f6ed0ed1f34fbb4d7a4b30
husseinkhaled <husseinkhaled733@gmail.com>
2025-05-10 14:59:51 +0300

    Implement pickup and dropoff point generation with caching support

--- internal/service/pickupdropoffservice/default_generator.go
+++ internal/service/pickupdropoffservice/default_generator.go
@@ -1,13 +1,29 @@
 package pickupdropoffservice
 
-import "matching-engine/internal/model"
+import (
+	"fmt"
+	"matching-engine/internal/enums"
+	"matching-engine/internal/geo"
+	"matching-engine/internal/model"
+)
 
 type DefaultGenerator struct {
+	processor geo.GeospatialProcessor
 }
 
-func NewDefaultSelector() *DefaultGenerator {
-	return &DefaultGenerator{}
+func NewDefaultSelector(processor geo.GeospatialProcessor) *DefaultGenerator {
+	return &DefaultGenerator{
+		processor: processor,
+	}
 }
 func (ds *DefaultGenerator) Generate(request *model.Request, offer *model.Offer) (pickup, dropoff *model.PathPoint, err error) {
-	return nil, nil, nil
+	if request == nil || offer == nil {
+		return nil, nil, fmt.Errorf("request or offer is nil")
+	}
+	// TODO: Implement the logic to generate pickup and dropoff points based on the walking time of the request
+	// Initially, we set the pickup time to the earliest departure time of the request
+	pickupPoint := model.NewPathPoint(request.Source(), enums.Pickup, request.EarliestDepartureTime(), request)
+	// The dropoff time is set to the latest arrival time of the request
+	dropoffPoint := model.NewPathPoint(offer.Destination(), enums.Dropoff, request.LatestArrivalTime(), offer)
+	return pickupPoint, dropoffPoint, nil
 }
