5e95cb4d6158c0776589b947ff9c0cfcfea102e3
MohamedAnwar121 <mohamedabdalrahman6@gmail.com>
2025-05-11 02:14:36 +0300

    Addressed review comments and added collections package

--- internal/geo/downsampling/timethreshold_downsampler.go
+++ internal/geo/downsampling/timethreshold_downsampler.go
@@ -9,16 +9,28 @@ import (
 	"time"
 )
 
+const DefaultInterval = 10 * time.Second
+
 type TimeThresholdDownSampler struct {
 	intervalAngle s1.Angle
 }
 
-func NewTimeThresholdDownSampler(interval time.Duration) *TimeThresholdDownSampler {
-	distMeters := geo.WalkingSpeedMPS * interval.Seconds()
-	angleRad := distMeters / geo.EarthRadiusInMeters
-	return &TimeThresholdDownSampler{
-		intervalAngle: s1.Angle(angleRad),
+type TimeThresholdDownSamplerOption func(*TimeThresholdDownSampler)
+
+func WithInterval(interval time.Duration) TimeThresholdDownSamplerOption {
+	return func(t *TimeThresholdDownSampler) {
+		t.intervalAngle = durationToAngle(interval)
 	}
+}
+
+func NewTimeThresholdDownSampler(opts ...TimeThresholdDownSamplerOption) *TimeThresholdDownSampler {
+	t := &TimeThresholdDownSampler{
+		intervalAngle: durationToAngle(DefaultInterval),
+	}
+	for _, opt := range opts {
+		opt(t)
+	}
+	return t
 }
 
 func (t *TimeThresholdDownSampler) DownSample(
@@ -32,10 +44,7 @@ func (t *TimeThresholdDownSampler) DownSample(
 		return route, nil
 	}
 
-	points := make([]s2.LatLng, n)
-	for i, pt := range route {
-		points[i] = s2.LatLngFromDegrees(pt.Lat(), pt.Lng())
-	}
+	points := convertCoordsToS2LatLngs(route)
 
 	out := make(model.LineString, 0, n/2)
 	out = append(out, route[0])
@@ -57,4 +66,17 @@ func (t *TimeThresholdDownSampler) DownSample(
 	}
 
 	return out, nil
+}
+
+func convertCoordsToS2LatLngs(route model.LineString) []s2.LatLng {
+	points := make([]s2.LatLng, len(route))
+	for i, pt := range route {
+		points[i] = s2.LatLngFromDegrees(pt.Lat(), pt.Lng())
+	}
+	return points
+}
+
+func durationToAngle(interval time.Duration) s1.Angle {
+	distance := geo.WalkingSpeedMPS * interval.Seconds()
+	return s1.Angle(distance / geo.EarthRadiusInMeters)
 }
