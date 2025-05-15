600d637713564bc0fce9a3748be8cb30f98f60fc
Mariam Elsamni <elsamnimariam@gmail.com>
2025-05-15 17:01:13 +0300

    Add walkingDuration & ID to pathPoint

--- internal/adapter/messaging/natsjetstream/dto/point_dto.go
+++ internal/adapter/messaging/natsjetstream/dto/point_dto.go
@@ -2,9 +2,10 @@ package dto
 
 // PointDTO is a Data Transfer Object for Point
 type PointDTO struct {
-	OwnerType string        `json:"ownerType"`
-	OwnerID   string        `json:"ownerID"`
-	Point     CoordinateDTO `json:"point"`
-	Time      string        `json:"time"`
-	PointType string        `json:"pointType"`
+	OwnerType              string        `json:"ownerType"`
+	OwnerID                string        `json:"ownerID"`
+	Point                  CoordinateDTO `json:"point"`
+	Time                   string        `json:"time"`
+	PointType              string        `json:"pointType"`
+	WalkingDurationMinutes int           `json:"walkingDurationMinutes"`
 }
