e3ddb907b4406bd2367d991a959a4ce542fa8f84
Mariam Elsamni <elsamnimariam@gmail.com>
2025-05-19 02:33:07 +0300

    Update path generator interface

--- internal/service/pathgeneration/generator/generator.go
+++ internal/service/pathgeneration/generator/generator.go
@@ -8,5 +8,8 @@ import (
 // PathGenerator defines the interface for path generation strategies
 type PathGenerator interface {
 	// GeneratePaths returns an iterator that produces all possible paths
-	GeneratePaths(path []model.PathPoint, pickup, dropoff *model.PathPoint) iter.Seq2[[]model.PathPoint, error]
+	GeneratePaths(
+		path []model.PathPoint,
+		pickup, dropoff *model.PathPoint,
+	) (iter.Seq2[[]model.PathPoint, error], error)
 }
