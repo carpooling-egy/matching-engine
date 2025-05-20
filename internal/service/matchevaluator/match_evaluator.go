71511223f4e3267e8408096e2b9ed0388a00d007
Mariam Elsamni <elsamnimariam@gmail.com>
2025-05-20 03:13:57 +0300

    fix

--- internal/service/matchevaluator/match_evaluator.go
+++ internal/service/matchevaluator/match_evaluator.go
@@ -33,9 +33,14 @@ func (m *MatchEvaluator) Evaluate(offerNode *model.OfferNode, requestNode *model
 	}
 
 	path, isFeasible, err := m.pathPlanner.FindFirstFeasiblePath(offerNode, requestNode)
-	if err != nil || len(path) < 2 || path == nil {
+	if err != nil {
 		return nil, false, fmt.Errorf("failed to find feasible path for offer %s and request %s: %w", offer.ID(), request.ID(), err)
 	}
+
+	if len(path) < 2 || path == nil {
+		return nil, false, fmt.Errorf("path is empty or has less than 2 points for offer %s and request %s", offer.ID(), request.ID())
+	}
+
 	if !isFeasible {
 		return nil, false, nil
 	}
