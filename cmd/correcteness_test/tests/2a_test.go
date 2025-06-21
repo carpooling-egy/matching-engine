a3537b5ddf7515aa87282b691b1f2d3bf9f831ef
husseinkhaled <husseinkhaled733@gmail.com>
2025-06-21 15:27:58 +0300

    Making durations in minutes instead of nanoseconds

--- cmd/correcteness_test/tests/2a_test.go
+++ cmd/correcteness_test/tests/2a_test.go
@@ -16,7 +16,7 @@ func getTest2a(engine routing.Engine) ([]*model.Offer, []*model.Request, map[str
 	offerSource, _ := model.NewCoordinate(31.2460735985739, 29.9744554984058)
 	offerDestination, _ := model.NewCoordinate(31.2068412085851, 29.9246876930902)
 	offerDepartureTime := correcteness_test.ParseTime("10:30")
-	offerDetourDuration := time.Duration(8)
+	offerDetourDuration := time.Duration(8) * time.Minute
 	offerCapacity := 1
 	offerCurrentNumberOfRequests := 1
 	offerSameGender := false
@@ -30,7 +30,7 @@ func getTest2a(engine routing.Engine) ([]*model.Offer, []*model.Request, map[str
 	matchedRequestDropoff, _ := model.NewCoordinate(31.208936, 29.933419)
 	matchedRequestEarliestDepartureTime := correcteness_test.ParseTime("10:20:00")
 	matchedRequestLatestArrivalTime := correcteness_test.ParseTime("11:20")
-	matchedRequestMaxWalkingDuration := time.Duration(0)
+	matchedRequestMaxWalkingDuration := time.Duration(0) * time.Minute
 	matchedRequestNumberOfRiders := 1
 	matchedRequestSameGender := true
 	matchedRequestGender := enums.Male
@@ -61,7 +61,7 @@ func getTest2a(engine routing.Engine) ([]*model.Offer, []*model.Request, map[str
 	requestDestination, _ := model.NewCoordinate(31.20773291, 29.92687263)
 	requestEarliestDepartureTime := correcteness_test.ParseTime("10:20:00")
 	requestLatestArrivalTime := correcteness_test.ParseTime("11:20")
-	requestMaxWalkingDuration := time.Duration(0)
+	requestMaxWalkingDuration := time.Duration(0) * time.Minute
 	requestNumberOfRiders := 1
 	requestSameGender := true
 	requestGender := enums.Male
