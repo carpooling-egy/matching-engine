19d82709b98534ad133e88abc7579e32f4e11fb0
Mariam Elsamni <elsamnimariam@gmail.com>
2025-06-22 00:17:50 +0300

    fix test 1e by overwriting latest arrival time of matched request

--- internal/model/request.go
+++ internal/model/request.go
@@ -43,12 +43,18 @@ func NewRequest(
 }
 
 // Getters
-func (r *Request) ID() string                               { return r.id }
-func (r *Request) UserID() string                           { return r.userID }
-func (r *Request) Source() *Coordinate                      { return &r.source }
-func (r *Request) Destination() *Coordinate                 { return &r.destination }
-func (r *Request) EarliestDepartureTime() time.Time         { return r.earliestDepartureTime }
-func (r *Request) LatestArrivalTime() time.Time             { return r.latestArrivalTime }
+func (r *Request) ID() string                       { return r.id }
+func (r *Request) UserID() string                   { return r.userID }
+func (r *Request) Source() *Coordinate              { return &r.source }
+func (r *Request) Destination() *Coordinate         { return &r.destination }
+func (r *Request) EarliestDepartureTime() time.Time { return r.earliestDepartureTime }
+func (r *Request) LatestArrivalTime() time.Time     { return r.latestArrivalTime }
+
+//  TODO: SetLatestArrivalTime is added for tests use only, we need to remove them and use a cleaner approach
+
+func (r *Request) SetLatestArrivalTime(t time.Time) {
+	r.latestArrivalTime = t
+}
 func (r *Request) MaxWalkingDurationMinutes() time.Duration { return r.maxWalkingDurationMinutes }
 func (r *Request) NumberOfRiders() int                      { return r.numberOfRiders }
 func (r *Request) Preferences() *Preference                 { return &r.preferences }
