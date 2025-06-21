bb76d94cea520b384b62f56a478134ab8bbe2e0e
Mariam Elsamni <elsamnimariam@gmail.com>
2025-06-21 03:19:27 +0300

    Add matcher initialization

--- cmd/correcteness_test/tests/correcteness_test.go
+++ cmd/correcteness_test/tests/correcteness_test.go
@@ -2,9 +2,12 @@ package tests
 
 import (
 	"context"
+	"go.uber.org/dig"
 	"matching-engine/cmd/correcteness_test"
 	"matching-engine/internal/adapter/routing"
 	"matching-engine/internal/adapter/valhalla"
+	"matching-engine/internal/app/di"
+	"matching-engine/internal/app/di/utils"
 	"matching-engine/internal/enums"
 	"matching-engine/internal/model"
 	matcher2 "matching-engine/internal/service/matcher"
@@ -19,159 +22,6 @@ type MatchedRequest struct {
 	pickupOrder  int
 	dropoffCoord *model.Coordinate
 	dropoffOrder int
-}
-
-func getTest1aiData(engine routing.Engine) ([]*model.Offer, []*model.Request, map[string]*model.MatchingResult) {
-	offers := make([]*model.Offer, 0)
-	requests := make([]*model.Request, 0)
-
-	// Create an offer with the specified attributes
-	offerSource, _ := model.NewCoordinate(31.2544088039743, 29.97376045816)
-	offerDestination, _ := model.NewCoordinate(31.20611644667, 29.9248733439259)
-	offerDepartureTime, _ := time.Parse("15:04", "10:30")
-	offerDetourDuration := time.Duration(30)
-	offerCapacity := 3
-	offerCurrentNumberOfRequests := 1
-	offerSameGender := false
-	offerGender := enums.Male
-	offerMaxEstimatedArrivalTime := getMaxEstimatedArrivalTime(*offerSource, *offerDestination, offerDepartureTime, offerDetourDuration, engine)
-
-	// Create a matched request for this offer
-	matchedRequestSource, _ := model.NewCoordinate(31.2544088, 29.97376046)
-	matchedRequestDestination, _ := model.NewCoordinate(31.20611645, 29.92487334)
-	matchedRequestPickup, _ := model.NewCoordinate(31.2544088, 29.97376046)
-	matchedRequestDropoff, _ := model.NewCoordinate(31.20611645, 29.92487334)
-	matchedRequestEarliestDepartureTime, _ := time.Parse("15:04:05", "10:20:00")
-	matchedRequestLatestArrivalTime, _ := time.Parse("15:04", "11:20")
-	matchedRequestMaxWalkingDuration := time.Duration(0)
-	matchedRequestNumberOfRiders := 1
-	matchedRequestSameGender := true
-	matchedRequestGender := enums.Male
-	matchedRequest := createRequest("2", "1", *matchedRequestSource, *matchedRequestDestination,
-		matchedRequestEarliestDepartureTime, matchedRequestLatestArrivalTime,
-		matchedRequestMaxWalkingDuration, matchedRequestNumberOfRiders,
-		matchedRequestGender, matchedRequestSameGender)
-	offerRequests := []*model.Request{matchedRequest}
-
-	offer := createOffer("1", "1", *offerSource, *offerDestination, offerDepartureTime,
-		offerDetourDuration, offerCapacity, offerCurrentNumberOfRequests, offerGender,
-		offerSameGender, offerMaxEstimatedArrivalTime, offerRequests)
-
-	// Create a matched request with pickup and dropoff coordinates
-	matchedReq := &MatchedRequest{
-		request:      matchedRequest,
-		pickupCoord:  matchedRequestPickup,
-		pickupOrder:  1,
-		dropoffCoord: matchedRequestDropoff,
-		dropoffOrder: 2,
-	}
-	offer.SetPath(createPath(offer, []*MatchedRequest{matchedReq}, engine))
-	// Add the offer to the list of offers
-	offers = append(offers, offer)
-
-	// Create another request
-	requestSource, _ := model.NewCoordinate(31.2544088, 29.97376046)
-	requestDestination, _ := model.NewCoordinate(31.20611645, 29.92487334)
-	requestEarliestDepartureTime, _ := time.Parse("15:04:05", "09:20:00")
-	requestLatestArrivalTime, _ := time.Parse("15:04", "10:20")
-	requestMaxWalkingDuration := time.Duration(0)
-	requestNumberOfRiders := 2
-	requestSameGender := true
-	requestGender := enums.Male
-
-	request := createRequest("3", "2", *requestSource, *requestDestination,
-		requestEarliestDepartureTime, requestLatestArrivalTime,
-		requestMaxWalkingDuration, requestNumberOfRiders,
-		requestGender, requestSameGender)
-
-	// Add the request to the list of requests
-	requests = append(requests, request)
-
-	// Create expected results
-	expectedResults := make(map[string]*model.MatchingResult)
-	return offers, requests, expectedResults
-}
-
-func getTest2a(engine routing.Engine) ([]*model.Offer, []*model.Request, map[string]*model.MatchingResult) {
-	offers := make([]*model.Offer, 0)
-	requests := make([]*model.Request, 0)
-
-	// Create an offer with the specified attributes
-	offerSource, _ := model.NewCoordinate(31.2460735985739, 29.9744554984058)
-	offerDestination, _ := model.NewCoordinate(31.2068412085851, 29.9246876930902)
-	offerDepartureTime, _ := time.Parse("15:04", "10:30")
-	offerDetourDuration := time.Duration(8)
-	offerCapacity := 1
-	offerCurrentNumberOfRequests := 1
-	offerSameGender := false
-	offerGender := enums.Male
-	offerMaxEstimatedArrivalTime := getMaxEstimatedArrivalTime(*offerSource, *offerDestination, offerDepartureTime, offerDetourDuration, engine)
-
-	// Create a matched request for this offer
-	matchedRequestSource, _ := model.NewCoordinate(31.22082087, 29.94795413)
-	matchedRequestDestination, _ := model.NewCoordinate(31.208936, 29.933419)
-	matchedRequestPickup, _ := model.NewCoordinate(31.22082087, 29.94795413)
-	matchedRequestDropoff, _ := model.NewCoordinate(31.208936, 29.933419)
-	matchedRequestEarliestDepartureTime, _ := time.Parse("15:04:05", "10:20:00")
-	matchedRequestLatestArrivalTime, _ := time.Parse("15:04", "11:20")
-	matchedRequestMaxWalkingDuration := time.Duration(0)
-	matchedRequestNumberOfRiders := 1
-	matchedRequestSameGender := true
-	matchedRequestGender := enums.Male
-	matchedRequest := createRequest("2", "1", *matchedRequestSource, *matchedRequestDestination,
-		matchedRequestEarliestDepartureTime, matchedRequestLatestArrivalTime,
-		matchedRequestMaxWalkingDuration, matchedRequestNumberOfRiders,
-		matchedRequestGender, matchedRequestSameGender)
-	offerRequests := []*model.Request{matchedRequest}
-
-	offer := createOffer("1", "1", *offerSource, *offerDestination, offerDepartureTime,
-		offerDetourDuration, offerCapacity, offerCurrentNumberOfRequests, offerGender,
-		offerSameGender, offerMaxEstimatedArrivalTime, offerRequests)
-
-	// Create a matched request with pickup and dropoff coordinates
-	matchedReq := &MatchedRequest{
-		request:      matchedRequest,
-		pickupCoord:  matchedRequestPickup,
-		pickupOrder:  1,
-		dropoffCoord: matchedRequestDropoff,
-		dropoffOrder: 2,
-	}
-	offer.SetPath(createPath(offer, []*MatchedRequest{matchedReq}, engine))
-	// Add the offer to the list of offers
-	offers = append(offers, offer)
-
-	// Create another request
-	requestSource, _ := model.NewCoordinate(31.208936, 29.933419)
-	requestDestination, _ := model.NewCoordinate(31.20773291, 29.92687263)
-	requestEarliestDepartureTime, _ := time.Parse("15:04:05", "10:20:00")
-	requestLatestArrivalTime, _ := time.Parse("15:04", "11:20")
-	requestMaxWalkingDuration := time.Duration(0)
-	requestNumberOfRiders := 1
-	requestSameGender := true
-	requestGender := enums.Male
-
-	request := createRequest("3", "2", *requestSource, *requestDestination,
-		requestEarliestDepartureTime, requestLatestArrivalTime,
-		requestMaxWalkingDuration, requestNumberOfRiders,
-		requestGender, requestSameGender)
-
-	// Add the request to the list of requests
-	requests = append(requests, request)
-
-	// Create expected results
-	expectedResults := make(map[string]*model.MatchingResult)
-	pickupPoint, dropoffPoint := computeRequestPickupDropoffPoints(engine, offer, requestSource, requestMaxWalkingDuration, requestDestination, requestEarliestDepartureTime, request, requestLatestArrivalTime)
-	pickupOrder, dropoffOrder := 4, 5
-	offerPath := addPointsToPath(engine, offer, pickupOrder, dropoffOrder, pickupPoint, dropoffPoint)
-
-	expectedResults[offer.ID()] = model.NewMatchingResult(
-		offer.ID(),
-		offer.UserID(),
-		[]*model.Request{request},
-		offerPath,
-		offer.CurrentNumberOfRequests()+1,
-	)
-	return offers, requests, expectedResults
 }
 
 func TestCorrecteness(t *testing.T) {
@@ -194,8 +44,7 @@ func TestCorrecteness(t *testing.T) {
 		},
 	}
 
-	// TODO: Create a matcher
-	matcher := matcher2.NewMatcher(nil, nil, nil, nil)
+	c := initializeMatchingService()
 
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
@@ -203,7 +52,7 @@ func TestCorrecteness(t *testing.T) {
 			if len(offers) == 0 || len(requests) == 0 {
 				t.Fatalf("No offers or requests generated for test %s", tt.name)
 			}
-			results, err := matcher.Match(offers, requests)
+			results, err := runMatcher(c, offers, requests)
 			if err != nil {
 				t.Fatalf("Matcher failed for test %s: %v", tt.name, err)
 			}
@@ -388,4 +237,31 @@ func calculateExpectedArrivalTimes(path []model.PathPoint, departureTime time.Ti
 		p.SetExpectedArrivalTime(departureTime.Add(drivingTimes[i]))
 	}
 	return path
+}
+
+func runMatcher(c *dig.Container, offers []*model.Offer, requests []*model.Request) ([]*model.MatchingResult, error) {
+	var matches []*model.MatchingResult
+	var matchErr error
+	err := c.Invoke(func(matcher *matcher2.Matcher) {
+		matches, matchErr = matcher.Match(offers, requests)
+	})
+	if err != nil {
+		panic("Failed to invoke matcher in the container: " + err.Error())
+	}
+	return matches, matchErr
+}
+
+func initializeMatchingService() *dig.Container {
+	c := dig.New()
+
+	// register all dependencies for matching services
+	di.RegisterGeoServices(c)
+	di.RegisterPickupDropoffServices(c)
+	di.RegisterTimeMatrixServices(c)
+	di.RegisterPathServices(c)
+	di.RegisterCheckers(c)
+	di.RegisterMatchingServices(c)
+	utils.Must(c.Provide(valhalla.NewValhalla))
+
+	return c
 }
