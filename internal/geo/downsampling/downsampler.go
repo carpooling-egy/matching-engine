f042071b5198a5fc3f3b5d1d29d06309589d523d
MohamedAnwar121 <mohamedabdalrahman6@gmail.com>
2025-06-26 19:05:16 +0300

    Added support for control vars for downsampling and pruning in the geo processor via env vars

--- internal/geo/downsampling/downsampler.go
+++ internal/geo/downsampling/downsampler.go
@@ -1,6 +1,7 @@
 package downsampling
 
 import (
+	"github.com/rs/zerolog/log"
 	"matching-engine/internal/model"
 )
 
@@ -13,5 +14,6 @@ type NoOpDownSampler struct{}
 var _ RouteDownSampler = (*NoOpDownSampler)(nil)
 
 func (NoOpDownSampler) DownSample(route model.LineString) (model.LineString, error) {
+	log.Debug().Msg("NoOpDownSampler.DownSample called")
 	return route, nil
 }
